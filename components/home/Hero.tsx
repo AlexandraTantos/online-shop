af0b18bc521d1da9848aa5c74163d53413eea4c8
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-07-18 19:33:28 +0300

    Add Hero Component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import HeroCarousel from "./HeroCarousel";
 
-export default function Hero() {
-  return <div>Hero</div>;
+function Hero() {
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          Where comfort meets design
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Discover curated furniture pieces that bring comfort and style to your
+          home.Shop our featured collection and elevate your everyday living.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
+export default Hero;
