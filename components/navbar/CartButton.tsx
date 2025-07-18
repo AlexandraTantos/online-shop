438c1034b079675718173caa5b2a20ba24932356
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-07-18 15:34:31 +0300

    Add complete Navbar with logo, search, cart and theme toggle

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,5 +1,22 @@
 import React from "react";
-
-export default function CartButton() {
-  return <div>CartButton</div>;
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { LuShoppingCart } from "react-icons/lu";
+export default async function CartButton() {
+  const numItems = 9;
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItems}
+        </span>
+      </Link>
+    </Button>
+  );
 }
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { HiHome } from "react-icons/hi";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <HiHome className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
