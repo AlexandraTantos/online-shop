189216358cf5e1d4f80ca4b2d45d6365c529ffb4
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-07-19 14:13:27 +0300

    Add Products Page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,13 @@
-const ProductsPage = () => {
-  return <div>Store Starter</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+const ProductsPage = ({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) => {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
 };
 export default ProductsPage;
