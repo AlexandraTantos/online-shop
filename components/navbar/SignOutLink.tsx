926c629ca361d62c696758e1eeb9ef47a99dc79f
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-07-21 14:30:35 +0300

    Implement user authentication using Clerk

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,21 @@
+"use client";
 import React from "react";
+import { useToast } from "../ui/use-toast";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+
+  const handleLogout = () => {
+    toast({ description: "Logout Successful" });
+  };
+
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 }
